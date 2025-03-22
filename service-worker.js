15595241779fac92ea4c8eaeb0acee514ced8020
Preston Chen <chen.preston@gmail.com>
2025-03-23 12:40:30 +1300

    v0.1.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "counting-helper-cache-v0.1.0"; // 更新缓存时修改版本号                                      
+const CACHE_NAME = "counting-helper-cache-v0.1.1"; // 更新缓存时修改版本号                                      
 const urlsToCache = [
   "/",
   "./index.html",
